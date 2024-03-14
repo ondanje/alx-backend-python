d1c09f387de5c12be6448318035a5a1155165a0a
ondanje <cyrilondanje@gmail.com>
2024-03-14 19:09:32 +0300

    concated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+type-annotated function concat that takes a string str1 and
+a string str2 as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """the return type is a string that has been concated"""
+    return str1 + str2
