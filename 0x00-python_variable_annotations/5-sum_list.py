f6daf257ef48233101dc86bea2858a91d4ff0000
ondanje <cyrilondanje@gmail.com>
2024-03-14 22:22:35 +0300

    variable annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+type-annotated function sum_list which takes a list
+input_list of floats as argument and returns their sum as a float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    looping over the list using a for loop and the adding the sum
+    of the items of the list
+    """
+    sum = 0
+    for x in range(len(input_list)):
+        """print(input_list[x])"""
+
+        sum += input_list[x]
+
+    return sum
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+type-annotated function sum_mixed_list which takes a list
+mxd_lst of integers and floats and returns their sum as a float.
+"""
+
+
+def sum_mixed_list(mxd_list: list[int | float]) -> float:
+    """
+    using the | operator to indicate that the type
+    could be either int or float
+    """
+    sum = 0
+
+    for x in range(len(mxd_list)):
+        """print(mxd_list[x])"""
+
+        sum += mxd_list[x]
+
+    return sum
